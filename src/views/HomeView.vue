68b2ee4c6badcb1515b7b4251c4e041bb236a502
Vue Workshop <example@example.com>
2019-09-05 13:57:17 -0500

    added cards to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,15 @@
 <template>
     <div id="HomeView">
-        Hello, I'm home
+        <v-card>
+            <v-icon color="red" size="50px">contacts</v-icon>
+            <h1>Friends</h1>
+            <h1>100</h1>
+        </v-card>
+        <v-card>
+            <v-icon color="red" size="50px">favorite</v-icon>
+            <h1>Favorites</h1>
+            <h1>1</h1>
+        </v-card>
     </div>
 </template>
 
