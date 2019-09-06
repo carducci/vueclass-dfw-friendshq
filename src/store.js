8d2aaec5d2c80363a371c82592935f0e9162f5f9
Vue Workshop <example@example.com>
2019-09-06 11:57:44 -0500

    Working axios call from state refactor

--- src/store.js
+++ src/store.js
@@ -7,22 +7,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    friends: [
-      {
-        "firstName": "Matt",
-        "lastName": "Forsythe",
-        "gender": "none",
-        "fav": false,
-        "id": 3
-      },
-      {
-        "id": 4,
-        "firstName": "Nate",
-        "lastName": "Schutta",
-        "gender": "male",
-        "fav": true
-      }
-    ]
+    friends: []
   },
   actions: {
     loadData({ commit }) {
